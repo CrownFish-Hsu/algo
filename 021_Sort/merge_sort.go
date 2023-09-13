63c65997f5b6c3e1d77bdad4e70c9b6f4d14ae8d
CrownFish-Hsu <xuchuyuan@yeah.net>
2023-09-13 23:34:44 +0800

    add merge sort non-recursion

--- 021_Sort/merge_sort.go
+++ 021_Sort/merge_sort.go
@@ -1,9 +1,12 @@
 package main
 
+// https://leetcode.cn/problems/sort-an-array/
 // 空间复杂度为O(N) 需要借助一个额外数组
 func sortArray(nums []int) []int {
 	if len(nums) > 0 {
 		mergeSort1(nums, 0, len(nums)-1)
+
+		mergeSort2(nums)
 	}
 
 	return nums
@@ -24,6 +27,36 @@ func mergeSort1(nums []int, l int, r int) {
 	mergeSort1(nums, l, mid)
 	mergeSort1(nums, mid+1, r)
 	merge(nums, l, r, mid)
+}
+
+/**
+ * 迭代版本:
+ * 每次设置一个步长 step 1,2,4,8,16....直到N
+ *
+ */
+func mergeSort2(nums []int) {
+	// for 总共O(logN)次
+	n := len(nums)
+	for step := 1; step < n; step *= 2 {
+		l, r := 0, 0
+		// for 执行O(N)次
+		for l < n {
+			mid := l + step - 1
+			if mid >= n-1 {
+				//已经没有右侧数字
+				break
+			}
+
+			if l+2*step > n {
+				r = n - 1
+			} else {
+				r = l + 2*step - 1
+			}
+
+			merge(nums, l, r, mid)
+			l = r + 1
+		}
+	}
 }
 
 func merge(nums []int, l int, r int, mid int) {
